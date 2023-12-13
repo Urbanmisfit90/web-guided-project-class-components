55862c878dfcf096b3d9f6b10bcf92e4b69f5c8e
Robert Dixon <robmsc24@gmail.com>
2023-12-13 13:38:18 -0500

    Update changes

--- followAlong/src/components/GroceryList.js
+++ followAlong/src/components/GroceryList.js
@@ -1,16 +1,21 @@
+//GroceryList
+
 import React from "react";
 
 import Item from "./Item";
 
-const GroceryList = props => {
+const GroceryList = (props) => {
   // for sorting the list based on whether an item has been purchased or not
   // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased);
   return (
     <div className="shopping-list">
-      {props.groceries.map(item => (
-        <Item key={item.id} item={item} />
+      {props.groceries.map((item) => (
+        <Item key={item.id} item={item} toggleItem={props.toggleItem} />
       ))}
-    </div>
+        <button className="clear-btn" onClick={props.clearPurchased}>
+          Clear Purchased
+        </button>
+      </div>
   );
 };
 
